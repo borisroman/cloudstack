51698f06ab6886965a2bcbe66d22c07e2e0785e1
Boris Schrijver <bschrijver@schubergphilis.com>
2016-01-14 14:49:58 +0100

    Restart DHCP server after files have been committed

--- systemvm/patches/debian/config/opt/cloud/bin/cs/CsDhcp.py
+++ systemvm/patches/debian/config/opt/cloud/bin/cs/CsDhcp.py
@@ -50,11 +50,11 @@ class CsDhcp(CsDataBag):
 
         self.configure_server()
 
-        # We restart DNSMASQ every time the configure.py is called in order to avoid lease problems.
-        CsHelper.service("dnsmasq", "restart")
-
         self.conf.commit()
         self.cloud.commit()
+
+        # We restart DNSMASQ every time the configure.py is called in order to avoid lease problems.
+        CsHelper.service("dnsmasq", "restart")
 
     def configure_server(self):
         # self.conf.addeq("dhcp-hostsfile=%s" % DHCP_HOSTS)
